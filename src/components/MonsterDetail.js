21ee988bf018caec56151cf7c03d225a2c3f8446
mattnorris1990 <mattnorris1990@gmail.com>
2022-05-13 19:34:02 +0100

    finished first draft of stat block detail

--- src/components/MonsterDetail.js
+++ src/components/MonsterDetail.js
@@ -1,6 +1,38 @@
 import React from "react"
 
 const MonsterDetail = ({mon}) => {
+    
+
+    const proficiencyItems = mon.proficiencies.map((res) => {
+        return res.proficiency.name + ", "
+    }) 
+
+    const condImmunityItems = mon.condition_immunities.map((res) => {
+        return res.name + ", "
+    }) 
+
+    const specialAbilityItems = mon.special_abilities.map((res) => {
+        return <p><b>{res.name}</b>: {res.desc}</p>
+    })
+
+    const actionItems = mon.actions.map((res) => {
+        return (<p>
+                    <b>{res.name}</b>: 
+                    {res.desc}
+                </p>
+        )
+
+    })
+
+    const legActionItems = mon.legendary_actions.map((res) => {
+        return (<p>
+                    <b>{res.name}</b>: 
+                    {res.desc}
+                </p>
+        )
+
+    })
+
 
 
     return (
@@ -13,55 +45,57 @@ const MonsterDetail = ({mon}) => {
                 <p><b>Hit Points</b> {mon.hit_points} ({mon.hit_dice})</p>
                 <p>
                     <b>Speed</b> {mon.speed.walk}
-                    {mon.speed.hasOwnProperty('swim') ? "," + mon.speed.swim + " (swim) " : null}
-                    {mon.speed.hasOwnProperty('fly') ? "," + mon.speed.fly + " (fly) " : null}
-                    {mon.speed.hasOwnProperty('burrow') ? "," + mon.speed.burrow + " (burrow) " : null}
-                    {mon.speed.hasOwnProperty('climb') ? "," + mon.speed.climb + " (climb)" : null}
+                    {mon.speed.hasOwnProperty('swim') ? ", " + mon.speed.swim + " (swim)" : null}
+                    {mon.speed.hasOwnProperty('fly') ? ", " + mon.speed.fly + " (fly)" : null}
+                    {mon.speed.hasOwnProperty('burrow') ? ", " + mon.speed.burrow + " (burrow)" : null}
+                    {mon.speed.hasOwnProperty('climb') ? ", " + mon.speed.climb + " (climb)" : null}
                 </p>
                 <hr />
                 <div className ="block-stats">
                     <div className="stat">
-                        <p className= "stat-name">Str</p> <p className= "stat-value">{mon.strength}</p>
+                        <p className= "stat-name">STR</p> <p className= "stat-value">{mon.strength}</p>
                     </div>
 
                     <div className="stat">
-                        <p className= "stat-name">Dex</p> <p className= "stat-value">{mon.dexterity}</p>
+                        <p className= "stat-name">DEX</p> <p className= "stat-value">{mon.dexterity}</p>
                     </div>
 
                     <div className="stat">
-                        <p className= "stat-name">Con</p> <p className= "stat-value">{mon.constitution}</p>
+                        <p className= "stat-name">CON</p> <p className= "stat-value">{mon.constitution}</p>
                     </div>
 
                     <div className="stat">
-                        <p className= "stat-name">Int</p> <p className= "stat-value">{mon.intelligence}</p>
+                        <p className= "stat-name">INT</p> <p className= "stat-value">{mon.intelligence}</p>
                     </div>
 
                     <div className="stat">
-                        <p className= "stat-name">Wis</p> <p className= "stat-value">{mon.wisdom}</p>
+                        <p className= "stat-name">WIS</p> <p className= "stat-value">{mon.wisdom}</p>
                     </div>
 
                     <div className="stat">
-                        <p className= "stat-name">Cha</p> <p className= "stat-value">{mon.charisma}</p>
+                        <p className= "stat-name">CHA</p> <p className= "stat-value">{mon.charisma}</p>
                     </div>
                 </div>
                 <hr />
                 <p>
-                {mon.proficiencies ? "Proficiences: " + "yes" : null}
+                {mon.proficiencies.length ? "Proficiences: " + proficiencyItems : null}
+                </p>
+                
+                <p>
+                {mon.damage_vulnerabilities.length ? "Damage Vulnerabilities: " + mon.damage_vulnerabilities[0] : null}
                 </p>
                 <p>
-                {mon.damage_vulnerabilities ? "Damage Vulnerabilities: " + "yes" : null}
+                {mon.damage_resistances.length ? "Damage Resistances: " + mon.damage_resistances : null}
                 </p>
                 <p>
-                {mon.damage_resistances ? "Damage Resistances: " + "yes" : null}
+                {mon.damage_immunities.length ? "Damage Immunities: " + mon.damage_immunities : null}
                 </p>
                 <p>
-                {mon.damage_immunities ? "Damage Immunities: " + "yes" : null}
+                {mon.condition_immunities.length ? "Condition Immunities: " + condImmunityItems : null}
                 </p>
                 <p>
-                {mon.condition_immunities ? "Condition Immunities: " + "yes" : null}
-                </p>
-                <p>
-                {mon.senses ? "senses: " + "yes" : null}
+                {/* THIS ONE IS NOT WORKING */}
+                {mon.senses.length ? "senses: " + mon.senses : null}
                 </p>
                 {mon.languages ? "Languages: " + mon.languages : "languages: -"}
                 <p>
@@ -69,6 +103,20 @@ const MonsterDetail = ({mon}) => {
                 </p>
 
                 <hr />
+
+                {mon.special_abilities.length ? <h3>Special Abilities</h3> : null}
+                {mon.special_abilities.length ? specialAbilityItems : null}
+                {mon.special_abilities.length ? <hr/> : null}
+
+                {mon.actions.length ? <h3>Actions</h3> : null}
+                {mon.actions.length ? <p>{actionItems}</p> : null}
+                {mon.actions.length ? <hr/> :null }                
+
+                {mon.legendary_actions.length ? <h3>Legendary Actions</h3> : null}
+                {mon.legendary_actions.length ? <p>{legActionItems}</p> : null}
+                {mon.legendary_actions.length ? <hr/> : null}
+
+                
 
 
                 
